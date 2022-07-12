1d7c3a65c953aa99748d0632ac6288822fc6a618
Somló Panna - ALLWIN Informatika Kft <somlo.panna@allwin.hu>
2022-07-12 16:35:02 +0200

    First commit

---
+++ js/controllers/MainController.js
@@ -0,0 +1,17 @@
+
+app.controller('MainController', ['$scope', function($scope) { 
+    $scope.todo = {
+        title: "Things I Need to Do",
+        list: ["Clean my room", "Go to the store", "Study Cracking the Coding Interview"]
+    }
+
+    $scope.books = {
+        title: "Books I Need to Buy",
+        list: []
+    }
+  
+  $scope.addItem = function(itemList, item) {
+    itemList.push(item)
+    }
+
+}]);
