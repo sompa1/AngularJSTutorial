46212ba24fe5a3c8c47b48c3d788eddde0f1a4a0
Somló Panna - ALLWIN Informatika Kft <somlo.panna@allwin.hu>
2022-07-13 09:18:47 +0200

    add api request

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,5 +1,5 @@
 
-app.controller('MainController', ['$scope', function($scope) { 
+app.controller('MainController', ['$scope', '$http', function($scope, $http) { 
     $scope.todo = {
         title: "Things I Need to Do",
         list: ["Clean my room", "Go to the store", "Study Cracking the Coding Interview"]
@@ -9,9 +9,25 @@ app.controller('MainController', ['$scope', function($scope) {
         title: "Books I Need to Buy",
         list: []
     }
-  
-  $scope.addItem = function(itemList, item) {
-    itemList.push(item)
-    }
+  function hasOnlyNumbers(item) {
+    return /^[0-9]*$/.test(item)
+  }
 
+  $scope.addItem = function(itemList, item) {
+    // ISBN: 10 or 13 length and consist of only numbers
+    if ((item.length == 10 || item.length == 13) && hasOnlyNumbers(item)) {
+        console.log("ISBN");
+        $http({
+            method: 'GET',
+            url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + item
+         }).then(function (response){
+            itemList.push("Title: " + response.data.items[0].volumeInfo.title + " // Author(s): " + response.data.items[0].volumeInfo.authors);
+         },function (error){
+            console.log(error, 'can not get data.');
+         });
+    } else {
+        console.log("not an ISBN");
+        itemList.push(item);
+    }
+    }
 }]);
